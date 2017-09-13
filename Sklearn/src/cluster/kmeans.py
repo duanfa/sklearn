f0beb5b1a4d91249c35f4e1ea05a1e8385c83668
duanfa <385104182@qq.com>
2017-09-13 18:52:05 +0800

    kmeans example

---
+++ Sklearn/src/cluster/kmeans.py
@@ -0,0 +1,10 @@
+from sklearn.cluster import KMeans
+import numpy as np
+X = np.array([[1, 2], [1, 4], [1, 0], [4, 2], [4, 4], [4, 0]])
+kmeans = KMeans(n_clusters=2, random_state=0).fit(X)
+labels = kmeans.labels_
+result = kmeans.predict([[0, 0], [4, 4]])
+centers = kmeans.cluster_centers_
+print(labels)
+print(result)
+print(centers)
